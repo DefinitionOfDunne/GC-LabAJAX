e41381c0a3415313081dcd2d147d6ee34654cd03
Dylan Dunne <dylanadunne@gmail.com>
2017-08-01 00:32:31 -0400

    design and script updates

--- app.js
+++ app.js
@@ -4,16 +4,29 @@ $(function() {
     $.get("https://www.reddit.com/r/aww/.json", function(req) {
             var dataChild = req.data.children;
             var randomized = dataChild[Math.floor(Math.random() * dataChild.length)];
-            console.log(randomized.data.title);
-            console.log(randomized.data.subreddit);
+            var title = randomized.data.title;
+            var channel = randomized.data.subreddit;
+            var author = randomized.data.author;
+            var image = randomized.data.preview.images[0].source.url;
+            $('#data-image').attr("src", image).append(image);
+            $('#data-content').append(title);
+            $('#data-subtitle').append(channel);
+            $('#data-author').append("Posted by " + author);
         })
         .done(function() {
-            alert("success");
+            console.log("success");
         })
         .fail(function() {
-            alert("error");
+            console.log("error");
         })
         .always(function() {
-            alert("finished");
+            console.log("finished");
         });
+
+    $('#data-image').mouseenter(function(event) {
+        $(this).addClass("animated pulse");
+    });
+    $('#data-image').mouseleave(function(event) {
+        $(this).removeClass("animated pulse");
+    });
 });
