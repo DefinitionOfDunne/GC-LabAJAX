f2a12a1c7399cc15128ac881eb05e9d1d428dfdd
Dylan Dunne <dylanadunne@gmail.com>
2017-08-01 23:54:47 -0400

    improved loop to handle errors

--- app.js
+++ app.js
@@ -2,29 +2,31 @@
 
 $(function() {
 
-    $('#page-title').addClass('animated bounceInLeft');
-
     $.get("https://www.reddit.com/r/aww/.json", function(req) {
             var dataChild = req.data.children;
-            // dataChild.forEach(function(item) {
-            //     if (item.data.preview.images[0].source.url === "undefined") {
-            //         console.log(item);
-            //     }
-            // })
-            var randomized = dataChild[Math.floor(Math.random() * dataChild.length)];
-            var title = randomized.data.title;
-            var channel = randomized.data.subreddit;
-            var author = randomized.data.author;
-            var image = randomized.data.preview.images[0].source.url;
-            var comments = randomized.data.num_comments;
-            var permalink = 'https://www.reddit.com' + randomized.data.permalink;
-            $('#data-image').attr("src", image).append(image).on("error", function() {});
-            $('#data-content').append(title);
-            // $('#data-subtitle').append(channel);
-            $('#data-author').append("Posted by " + author);
-            $('#comments-link').attr('href', permalink);
-            $('#comments').append(comments);
+
+            $('#data-image').on("error", function() {
+                for (var i = 0; i <= dataChild.length; i++) {
+                    dataChild.splice(i, 1);
+                    clearInfo();
+                }
+                var randomized = dataChild[Math.floor(Math.random() * dataChild.length)];
+
+                $('#data-image').attr("src", randomized.data.preview.images[0].source.url).append(randomized.data.preview.images[0].source.url).on("error", function() {});
+                $('#data-content').append(randomized.data.title);
+                $('#data-author').append("Posted by " + randomized.data.author);
+                $('#comments-link').attr('href', 'https://www.reddit.com' + randomized.data.permalink);
+                $('#comments').append(randomized.data.num_comments);
+            })
+                var randomized = dataChild[Math.floor(Math.random() * dataChild.length)];
+
+                $('#data-image').attr("src", randomized.data.preview.images[0].source.url).append(randomized.data.preview.images[0].source.url).on("error", function() {});
+                $('#data-content').append(randomized.data.title);
+                $('#data-author').append("Posted by " + randomized.data.author);
+                $('#comments-link').attr('href', 'https://www.reddit.com' + randomized.data.permalink);
+                $('#comments').append(randomized.data.num_comments);
         })
+
         .done(function() {
             console.log("success");
         })
@@ -35,10 +37,26 @@ $(function() {
             console.log("finished");
         });
 
+
+
+    // HELPER FUNCTIONS
+
+    function clearInfo() {
+        $('#comments').empty();
+        $('#data-author').empty();
+        $('#data-content').empty();
+    }
+
+
+    // ANIMATIONS
+
+
+
     $('#data-image, #comments').mouseenter(function(event) {
         $(this).addClass("animated pulse");
     });
     $('#data-image, #comments').mouseleave(function(event) {
         $(this).removeClass("animated pulse");
     });
+    $('#page-title').addClass('animated bounceInLeft');
 });
